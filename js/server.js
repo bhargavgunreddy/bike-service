e31d63842d5bc26ec6b720505a2b9721c9311e0a
Bhargav Gunreddy <gbhargavreddy@gmail.com>
2016-08-24 14:06:52 -0600

    adding js folder to keep js files

--- js/server.js
+++ js/server.js
@@ -12,10 +12,17 @@ var express = require('express');
 bikeapp = express();
 bikeapp.use(express.static(__dirname));
 
-bikeapp.get('/home', function(req, res) {
-	console.log("home page -->");
-        res.sendFile(__dirname + '/index.html'); // path must be absolute
+bikeapp.get('/', function(req, res, next) {
+	console.log("redirect to landing page -->");
+        //next();
+		res.send(' Use /home to redirect to landing page'); // path must be absolute
     });
+
+
+// bikeapp.get('/home', function(req, res) {
+	// console.log("home page -->");
+        // res.sendFile(__dirname + '/index.html'); // path must be absolute
+    // });
 
 // create a server on port 3000
 bikeapp.listen(3000);
